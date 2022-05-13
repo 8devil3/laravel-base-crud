55087e3ac3a479f6235c89f4657a0efcf780f9ef
8devil3 <8dev.il3@gmail.com>
2022-05-13 16:50:16 +0200

    aggiunta validazione

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -12,7 +12,7 @@
 
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="@error('title') is-invalid @enderror" name="title" aria-describedby="title" value="{{ $comic['title'] }}">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" aria-describedby="title" value="{{ old('title') }}">
 
                 @error('title')
                     <div class="alert alert-danger">{{ $message }}</div>
@@ -21,39 +21,59 @@
 
             <div class="mb-3 d-flex flex-column">
                 <label for="description" class="form-label">Description</label>
-                <textarea name="description" cols="30" rows="10" class="form-control" aria-describedby="description">{{ $comic['description'] }}</textarea>
+                <textarea name="description" cols="30" rows="10" class="form-control" aria-describedby="description">{{ old('description') }}</textarea>
             </div>
 
             <div class="mb-3">
                 <label for="thumb" class="form-label">Cover</label>
-                <input type="text" class="form-control" name="thumb" aria-describedby="cover" placeholder="Add image URL" value="{{ $comic['thumb'] }}">
+                <input type="text" class="form-control @error('thumb') is-invalid @enderror" name="thumb" aria-describedby="cover" placeholder="Add image URL" value="{{ old('thumb') }}>
+
+                @error('thumb')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="price" class="form-label">Price</label>
-                <input type="number" class="form-control" name="price" aria-describedby="price" step="0.01" min="0" placeholder="$ 0.00" value="{{ $comic['price'] }}">
+                <input type="number" class="form-control @error('price') is-invalid @enderror" name="price" aria-describedby="price" step="0.01" min="0" placeholder="$ 0.00" value="{{ old('price') }}">
+
+                @error('price')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
                 </div>
 
             <div class="mb-3">
                 <label for="series" class="form-label">Series</label>
-                <input type="text" class="form-control" name="series" aria-describedby="series" value="{{ $comic['series'] }}">
+                <input type="text" class="form-control @error('series') is-invalid @enderror" name="series" aria-describedby="series" value="{{ old('series') }}">
+
+                @error('series')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Sale date</label>
-                <input type="date" class="form-control" name="sale_date" aria-describedby="sale date" value="{{ $comic['sale_date'] }}">
+                <input type="date" class="form-control @error('sale_date') is-invalid @enderror" name="sale_date" aria-describedby="sale date" value="{{ old('sale_date') }}">
+
+                @error('sale_date')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="type" class="form-label">Type</label>
-                <select name="type" class="form-control" aria-describedby="type" value="{{ $comic['type'] }}">
+                <select name="type" class="form-control @error('type') is-invalid @enderror" aria-describedby="type" value="{{ old('type') }}">
                     <option value="comic">Comic</option>
                     <option value="graphic-novel">Graphic Novel</option>
                     <option value="poster">Poster</option>
                 </select>
+
+                @error('type')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
 
-            <button type="submit" class="btn btn-success">Save</button>
+            <button type="submit" class="btn btn-primary">Add</button>
 
             <div class="btn btn-secondary" id="btn-reset">Clear fields</div>
 
